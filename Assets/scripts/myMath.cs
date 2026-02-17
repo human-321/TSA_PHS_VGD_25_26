aa783cf1e0c0696a07748c6e9087bb958a4cab2d
human-321 <aidenpdonahue@gmail.com>
2026-02-16 22:37:00 -0500

    Add files via upload

--- Assets/scripts/myMath.cs
+++ Assets/scripts/myMath.cs
@@ -7,7 +7,9 @@ using UnityEngine;
 public struct myMath
 {
 
-    public static Vector3 toVec3(Vector2 v) {return new Vector3(v.x,v.y,0f);}
-    
+    public static Vector3    toVec3(Vector2 v) {return new Vector3(v.x,v.y,0f);}
+    public static Vector3    toVec2(Vector3 v) {return new Vector2(v.x,v.y);}
+    public static Vector2Int toVec2Int(Vector3 v) {return new Vector2Int((int)v.x,(int)v.y);}
+    public static float      glamFloatDisplay(float x) {return Mathf.Floor(x * 100)/100;}
 
 }
